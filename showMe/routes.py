24633beff8646a9cf959fc93f0d2cb4e4719f34a
Ardjan-Aalberts <ardjanaalberts@hotmail.com>
2017-03-01 17:10:48 +0100

    added form sending title and icon back to route.py

--- showMe/routes.py
+++ showMe/routes.py
@@ -1,10 +1,16 @@
 from flask import render_template, send_from_directory
+from flask import request
+
 from showMe import app
 import settings
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        new_title = request.form.get("title")
+        new_icon = request.form.get("sel_icon")
+        print new_icon, new_title
     return render_template("index.html")
 
 

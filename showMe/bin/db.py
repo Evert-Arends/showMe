0c9b86e64eedb08f84a51f58c15aae814296e12a
Ardjan-Aalberts <ardjanaalberts@hotmail.com>
2017-03-02 15:06:26 +0100

    added db.sqlite3 and db.py

---
+++ showMe/bin/db.py
@@ -0,0 +1,19 @@
+import sqlite3
+from showMe import app
+from flask import g
+
+DATABASE = '../../db.sqlite3'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
